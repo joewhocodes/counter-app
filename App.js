59df17d2a539c136a6882136aa0e6acf7b9f3715
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-09 09:16:47 +0200

    add increment component

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    value: 0,
+    total_taps: 0,
+  }
+
+  incrementValue = () => {
+    this.setState({
+      value: this.state.value + 1,
+      total_taps: this.state.total_taps + 1
+    })
+    console.log(`value ${this.state.value + 1}`)
+  }
+
+  decrementValue = () => {
+    this.setState({
+      value: this.state.value - 1,
+      total_taps: this.state.total_taps - 1
+    })
+    console.log(`value ${this.state.value - 1}`)
+  }
+  
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Hello World!</Text>
+                <StatusBar style='auto' />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
