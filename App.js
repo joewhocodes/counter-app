4df49e84611393d297526fb3b673750ba01e5724
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-09 09:23:14 +0200

    add increment and decrement counters

--- App.js
+++ App.js
@@ -1,34 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  state = {
-    value: 0,
-    total_taps: 0,
-  }
+    state = {
+        value: 0,
+        total_taps: 0,
+    };
 
-  incrementValue = () => {
-    this.setState({
-      value: this.state.value + 1,
-      total_taps: this.state.total_taps + 1
-    })
-    console.log(`value ${this.state.value + 1}`)
-  }
+    incrementValue = () => {
+        this.setState({
+            value: this.state.value + 1,
+            total_taps: this.state.total_taps + 1,
+        });
+        console.log(`value ${this.state.value + 1}`);
+    };
 
-  decrementValue = () => {
-    this.setState({
-      value: this.state.value - 1,
-      total_taps: this.state.total_taps - 1
-    })
-    console.log(`value ${this.state.value - 1}`)
-  }
-  
+    decrementValue = () => {
+        this.setState({
+            value: this.state.value - 1,
+            total_taps: this.state.total_taps - 1,
+        });
+        console.log(`value ${this.state.value - 1}`);
+    };
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hello World!</Text>
+                <Text style={{ fontSize: 60, marginBottom: -20 }}>{this.state.value}</Text>
+                <Text style={{ fontSize: 12, padding: 20, color: 'grey' }}>
+                    Total Taps: {this.state.total_taps}
+                </Text>
                 <StatusBar style='auto' />
+                <View style={{flexDirection: 'row'}}>
+                  <Button onPress={this.decrementValue} title='Decrease'/>
+                  <Text>  </Text>
+                  <Button onPress={this.incrementValue} title='Increase'/>
+                </View>
             </View>
         );
     }
